06334a1828a3366c448e2ede33594600b111dfa7
LSitao <121932158+LSitao@users.noreply.github.com>
2023-01-22 20:56:06 +0800

    Create pre_iu.sh

---
+++ pre_iu.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# author: https://t.me/iu536
+
+r00t=`echo $USER`
+if [ $r00t != "root" ]
+  then 
+      clear
+      echo "You are not root!"
+	  echo
+	  echo "请在root用户下执行该脚本!"
+	  exit
+fi
+
+if test -e /usr/iu/iu.sh
+ then 
+      iu
+ else 
+      mkdir -p /usr/iu
+	  
+	  curl https://raw.githubusercontent.com/LSitao/vless_gRPC_nginx_tls/main/iu.sh > /usr/iu/iu.sh
+	  
+	  cp /usr/iu/iu.sh /usr/bin/iu
+	  
+	  curl https://raw.githubusercontent.com/LSitao/vless_gRPC_nginx_tls/main/install.sh > /usr/iu/install.sh
+	  
+	  /usr/bin/iu
+	  
+fi
+	  
+	  
+	  
